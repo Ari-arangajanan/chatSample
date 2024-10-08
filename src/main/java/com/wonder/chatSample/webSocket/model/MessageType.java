c1012c2ac8f95bb818d644329d5780176c780f83
Arangajanan Sivasubramaniam <47890434+Ari-arangajanan@users.noreply.github.com>
2024-10-08 14:22:39 +0530

    presence

--- src/main/java/com/wonder/chatSample/webSocket/model/MessageType.java
+++ src/main/java/com/wonder/chatSample/webSocket/model/MessageType.java
@@ -1,1 +1,1 @@
-package com.wonder.chatSample.webSocket.model;public enum MessageType {    NEW_MESSAGE, JOIN_SUCCESS, LEAVE, ERROR, FORBIDDEN, ADD_CONTACT, GET_CONTACTS, PING, UNSUBSCRIBE}
+package com.wonder.chatSample.webSocket.model;public enum MessageType {    NEW_MESSAGE, JOIN_SUCCESS, LEAVE, ERROR, FORBIDDEN, ADD_CONTACT, GET_CONTACTS, PING, UNSUBSCRIBE, PRESENCE}
